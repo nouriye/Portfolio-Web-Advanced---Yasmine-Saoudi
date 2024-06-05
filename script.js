c7d608a1e57151a7967175c210a7d00d374251bb
nouriye <yasmine.saoudi@student.ehb.be>
2024-06-05 22:51:36 +0200

    javascript: elementen manipulatie, op hover en weg hover + projecten dynamisch toevoegen in een array en index gebruiken voor elementenmanipulatie

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Elementen selecteren
+    // Element selection
     const subscribeForm = document.getElementById('newsletter-form');
     const projectElements = document.querySelectorAll('.project img');
+    const banner = document.getElementById('main-banner');
+    const navbarTitle = document.getElementById('headertitle');
+    const projectTitles = [];
 
-    // Event aan elementen koppelen
-    projectElements.forEach(project => {
+    // Event listener for project image clicks
+    projectElements.forEach((project, index) => {
+        projectTitles.push(project.alt);
         project.addEventListener('click', () => {
             alert(`Je hebt geklikt op ${project.alt}`);
         });
-    });
-
-    subscribeForm.addEventListener('submit', (e) => {
-        e.preventDefault();
+        project.addEventListener('mouseover', () => {
+            // Elementen manipuleren: texten website aanpassen bij weghoveren van een project
+            banner.querySelector('h2').textContent = projectTitles[index];
+            banner.querySelector('p').textContent = `Dit is het omschrijving van ${projectTitles[index]}.`;
+            navbarTitle.textContent = `Mijn portfolio (${projectTitles[index]})`;
+        });
+        project.addEventListener('mouseleave', () => {
+            // Elementen manipuleren: texten website aanpassen bij aanklikken van een project
+            banner.querySelector('h2').textContent = "Welkom op mijn Portfolio";
+            banner.querySelector('p').textContent = `Hier kunt u al mijn fotografieprojecten zien.`;
+            navbarTitle.textContent = `Mijn portfolio`;
+        });
     });
 });
