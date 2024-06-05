6d75dffe00c478e57679a60795983a06ddf0e04b
nouriye <yasmine.saoudi@student.ehb.be>
2024-06-05 16:49:31 +0200

    event listener koppelen aan subscribtion form

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Elementen selecteren
+    const subscribeForm = document.getElementById('newsletter-form');
     const projectElements = document.querySelectorAll('.project img');
 
     // Event aan elementen koppelen
@@ -7,5 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
         project.addEventListener('click', () => {
             alert(`Je hebt geklikt op ${project.alt}`);
         });
+    });
+
+    subscribeForm.addEventListener('submit', (e) => {
+        e.preventDefault();
     });
 });
